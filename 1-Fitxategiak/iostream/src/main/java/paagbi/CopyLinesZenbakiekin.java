16091007b6d339cc797604e6f76afb66c5aa8cf9
AnderAzpiazu <148545080+AnderAzpiazu@users.noreply.github.com>
2024-09-16 12:13:52 +0200

    iostream bukatuta

--- 1-Fitxategiak/iostream/src/main/java/paagbi/CopyLinesZenbakiekin.java
+++ 1-Fitxategiak/iostream/src/main/java/paagbi/CopyLinesZenbakiekin.java
@@ -1,26 +1,25 @@
 package paagbi;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-public class CopyCharactersMayusculaz {
-    public static void main(String[] args) throws IOException {
+public class CopyLinesZenbakiekin {
+    public static void main(String[] args) throws IOException{
         BufferedReader inputStream = null;
-        BufferedWriter outputStream = null;
+        PrintWriter outputStream = null;
 
         try {
             inputStream = new BufferedReader(new FileReader("xanadu.txt"));
-            outputStream = new BufferedWriter(new FileWriter("xanaduMayusculaz.txt"));
-            
-            int c;
-            while ((c = inputStream.read()) != -1) {
-                if (c > 96 && c < 123) {
-                    c = c - 32;
-                }
-                outputStream.write(c);
+            outputStream = new PrintWriter(new FileWriter("characteroutput.txt"));
+
+            String l;
+            int count = 0;
+            while ((l = inputStream.readLine()) != null) {
+                count++;
+                outputStream.println(count+" "+l);
             }
         } finally {
             if (inputStream != null) {
